9c5cafb4e95e0e200848bba4057c76035372a5fa
William Blondel <wblondel@z2r5p1.le-101.fr>
2018-05-06 19:00:25 +0200

    added timer to test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@ mkdir -p results/mine_$nbtet
 for n in {1..500}
 do
 	echo "Processing file $n of 500"
-	./fillit samples/$nbtet/test$n > results/mine_$nbtet/test$n
+	time ./fillit samples/$nbtet/test$n > results/mine_$nbtet/test$n
+	echo ""
 done
